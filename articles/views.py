ead8aecd90235876faa8bc32513bd70409d14dc8
Vladimir <dev.method.mail@gmail.com>
2020-06-20 09:49:23 +0300

    Refactor material page, add css files

--- articles/views.py
+++ articles/views.py
@@ -3,29 +3,30 @@ from articles.models import NewArticle, ArticlesSeo, NewsSeo
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from articles.serializers import ArticlesSerializer
+from vekomet_redesign import settings
 # Create your views here.
 
 def articles(request):
     seo = ArticlesSeo.objects.all()
     materials_flag = "current"
-    return render(request, 'articles/dev/articles.html', {'materials_flag':materials_flag, 'seo': seo})
+    return render(request, 'articles/{}/articles.html'.format(settings.TEMP_PREFIX), {'materials_flag':materials_flag, 'seo': seo})
 
 
 def articles_item(request,slug):
     materials_flag = "current"
-    article=NewArticle.objects.filter(category_id=1).get(slug=slug)
-    return render(request, 'articles/dev/articles-item.html', {'article': article, 'materials_flag':materials_flag})
+    material=NewArticle.objects.filter(category_id=1).get(slug=slug)
+    return render(request, 'articles/{}/material-item.html'.format(settings.TEMP_PREFIX), {'material': material, 'materials_flag':materials_flag})
 
 def news(request):
     seo = NewsSeo.objects.all()
     materials_flag = "current"
-    return render(request, 'articles/dev/news.html', {'materials_flag':materials_flag, 'seo': seo})
+    return render(request, 'articles/{}/news.html'.format(settings.TEMP_PREFIX), {'materials_flag':materials_flag, 'seo': seo})
 
 
 def news_item(request,slug):
     materials_flag = "current"
-    article=NewArticle.objects.filter(category_id=2).get(slug=slug)
-    return render(request, 'articles/dev/news-item.html', {'article': article, 'materials_flag':materials_flag})
+    material=NewArticle.objects.filter(category_id=2).get(slug=slug)
+    return render(request, 'articles/{}/material-item.html'.format(settings.TEMP_PREFIX), {'material': material, 'materials_flag':materials_flag})
 
 @api_view(['GET'])
 def articles_collection(request):
